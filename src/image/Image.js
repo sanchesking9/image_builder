4f06db896fe65957d2d92a184766aea26a344459
Alex Sinner <sancheskinghtc@gmail.com>
2017-05-07 12:12:28 +0300

    removed unnecessary code

--- src/image/Image.js
+++ src/image/Image.js
@@ -65,43 +65,6 @@ export default class Image extends Component {
 
     canvas.add(out_frame);
     this.props.setFrame(out_frame);
-
-    canvas.on('object:added', function(e) {
-      out_frame.bringToFront();
-    });
-    canvas.on('object:moving', (e) => {
-      const obj = e.target;
-      let angleLeft = 0;
-      let realLeft = obj.left;
-      let diagonal = Math.sqrt(Math.pow(e.target.width * e.target.scaleX, 2) + Math.pow(e.target.height * e.target.scaleY, 2));
-      console.log(e.target, 'angle=',  e.target.angle, 'top=', obj.top, 'left=', obj.left, 'width=', e.target.width, 'height=', e.target.height);
-
-      if (e.target.angle < 46 && e.target.angle !== 0) {
-        angleLeft = (diagonal * (e.target.angle / 0.45 / 100)) / 2;
-      }
-      realLeft = realLeft - angleLeft;
-      console.log('realLeft=', realLeft, 'angleLeft=', angleLeft, 'obj.left=', obj.left, 'diagonal=', diagonal);
-
-      if (realLeft < 0) {
-        obj.setLeft(angleLeft);
-      }
-
-      if (obj.top < 0) {
-        obj.setTop(0);
-      }
-
-      if (obj.height * obj.scaleY + obj.top > e.target.canvas.height) {
-        const top = e.target.canvas.height - obj.height * obj.scaleY;
-        console.log('top', top);
-        top > 0 && obj.setTop(top);
-      }
-
-      if (obj.width * obj.scaleX + obj.left > e.target.canvas.width) {
-        const left = e.target.canvas.width - obj.width * obj.scaleX;
-        console.log('left', left);
-        left > 0 && obj.setLeft(left);
-      }
-    });
   }
 
   render() {
